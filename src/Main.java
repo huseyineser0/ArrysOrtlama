2ea37b49fb8f54a1e1d72d3585447c2ab8af0f4d
Hüseyin Eser <hsy_esr@hotmail.com>
2024-03-13 23:30:32 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int [] list={1,2,3,4,5};
+        double sum=0.0;
+        for (int i=0; i<list.length; i++){
+            sum+=list[i];
+        }
+        double ort=sum/list.length;
+        System.out.println(ort);
+
+    }
+}
